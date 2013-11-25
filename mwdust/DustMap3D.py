dec1b7d3de7f06bd637b7232b660c5bad7e634f2
Jo Bovy <bovy@ias.edu>
2013-11-24 19:40:30 -0500

    some first code to evaluate dust maps

--- mwdust/DustMap3D.py
+++ mwdust/DustMap3D.py
@@ -6,17 +6,19 @@
 ###############################################################################
 class DustMap3D:
     """top-level class for a 3D dust map; all other dust maps inherit from this"""
-    def __init__(self):
+    def __init__(self,filter=None):
         """
         NAME:
            __init__
         PURPOSE:
            Initialize the dust map
         INPUT:
+           filter= filter to return the extinction in when called
         OUTPUT:
         HISTORY:
            2013-11-24 - Started - Bovy (IAS)
         """
+        self._filter= filter
         return None
 
     def __call__(self,*args,**kwargs):
@@ -26,9 +28,14 @@ class DustMap3D:
         PURPOSE:
            evaluate the dust map
         INPUT:
+           Either:
+              (l,b,d) -  Galactic longitude, latitude (deg), and distance (kpc)
         OUTPUT:
         HISTORY:
            2013-11-24 - Started - Bovy (IAS)
         """
-        raise NotImplementedError("'__call__' for this DustMap3D not implemented yet")
-
+        if True: #(l,b,d)
+            try:
+                return self._evaluate(*args)
+            except AttributeError:
+                raise NotImplementedError("'_evaluate' for this DustMap3D not implemented yet")
