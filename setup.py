05207376220c9f74af5966bb605792ad77708064
Jo Bovy <bovy@ias.edu>
2013-12-09 14:45:58 -0500

    automatically download SFD maps

--- setup.py
+++ setup.py
@@ -7,7 +7,53 @@ import glob
 
 longDescription= ""
 
-#Download Drimmel data first
+#Download SFD maps
+_DOWNLOAD_SFD= False
+_SFD_URL_NGP= 'http://www.astro.princeton.edu/~schlegel/dust/dustpub/maps/SFD_dust_4096_ngp.fits'
+_SFD_URL_SGP= 'http://www.astro.princeton.edu/~schlegel/dust/dustpub/maps/SFD_dust_4096_sgp.fits'
+if _DOWNLOAD_SFD and sys.argv[1] in ('install'):
+    if os.getenv('DUST_DIR') is None:
+        raise IOError('Please define an environment variable DUST_DIR as a top-level directory for various dust maps\nIf using sudo, you may have to run sudo -E to propagate environment variables')
+    else:
+        print '\033[1m'+'Downloading SFD dust maps ...'+'\033[0m'
+
+        if not os.path.exists(os.path.join(os.getenv('DUST_DIR'),'maps',
+                                           'SFD_dust_4096_ngp.fits')):
+            if not os.path.exists(os.path.join(os.getenv('DUST_DIR'),'maps')):
+                os.mkdir(os.path.join(os.getenv('DUST_DIR'),'maps'))
+            try:
+                subprocess.check_call(['wget',_SFD_URL_NGP,'-O',
+                                       os.path.join(os.getenv('DUST_DIR'),'maps',
+                                                    'SFD_dust_4096_ngp.fits')])
+            except subprocess.CalledProcessError:
+                print '\033[1m'+"Downloading SFD dust-map data from %s failed ..." % _SFD_URL_NGP +'\033[0m'
+            try:
+                subprocess.check_call(['chown',os.getenv('SUDO_USER'),
+                                       os.path.join(os.getenv('DUST_DIR'),
+                                                    'maps',
+                                                    'SFD_dust_4096_ngp.fits')])
+            except subprocess.CalledProcessError:
+                print '\033[1m'+"Problem changing ownership of data file..."+'\033[0m'
+
+                
+        if not os.path.exists(os.path.join(os.getenv('DUST_DIR'),'maps',
+                                           'SFD_dust_4096_sgp.fits')):
+            try:
+                subprocess.check_call(['wget',_SFD_URL_SGP,'-O',
+                                       os.path.join(os.getenv('DUST_DIR'),'maps',
+                                                    'SFD_dust_4096_sgp.fits')])
+            except subprocess.CalledProcessError:
+                print '\033[1m'+"Downloading SFD dust-map data from %s failed ..." % _SFD_URL_SGP +'\033[0m'
+
+            try:
+                subprocess.check_call(['chown',os.getenv('SUDO_USER'),
+                                       os.path.join(os.getenv('DUST_DIR'),
+                                                    'maps',
+                                                    'SFD_dust_4096_sgp.fits')])
+            except subprocess.CalledProcessError:
+                print '\033[1m'+"Problem changing ownership of data file..."+'\033[0m'
+               
+#Download Drimmel data
 _DOWNLOAD_DRIMMEL= False
 _DRIMMEL_URL= 'ftp://ftp.oato.inaf.it/astrometria/extinction/data-for.tar.gz'
 if _DOWNLOAD_DRIMMEL \
@@ -18,7 +64,6 @@ if _DOWNLOAD_DRIMMEL \
                                    '-O','mwdust/util/drimmeldata/data-for.tar.gz'])
         except subprocess.CalledProcessError:
             print "Downloading Drimmel dust-map data from %s failed ..." % _DRIMMEL_URL
-            raise
         try:
             subprocess.check_call(['tar','xvzf',
                                    'mwdust/util/drimmeldata/data-for.tar.gz',
