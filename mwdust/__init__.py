d0594fece3d3c69a927c562fe9a535b18e18dad2
Jo Bovy <bovy@ias.edu>
2015-07-28 21:30:52 +0200

    add combined map from Bovy et al. (2015)

--- mwdust/__init__.py
+++ mwdust/__init__.py
@@ -3,4 +3,5 @@ from mwdust.Marshall06 import Marshall06
 from mwdust.Drimmel03 import Drimmel03
 from mwdust.Sale14 import Sale14
 from mwdust.Green15 import Green15
+from mwdust.Combined15 import Combined15
 from mwdust.Zero import Zero
