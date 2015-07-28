656f67dab3de404d0ab1ac8b08fbf835e3e15cdb
Jo Bovy <bovy@ias.edu>
2015-07-28 21:30:35 +0200

    re-write Green15 to be a subclass of the general hierarchical HEALPix class

--- mwdust/Green15.py
+++ mwdust/Green15.py
@@ -6,13 +6,10 @@
 import os, os.path
 import numpy
 import h5py
-from scipy import interpolate
-import healpy
-from mwdust.util.extCurves import aebv
-from DustMap3D import DustMap3D
+from mwdust.HierarchicalHealpixMap import HierarchicalHealpixMap
 _DEGTORAD= numpy.pi/180.
 _greendir= os.path.join(os.getenv('DUST_DIR'),'green15')
-class Green15(DustMap3D):
+class Green15(HierarchicalHealpixMap):
     """extinction model from Green et al. (2015)"""
     def __init__(self,filter=None,sf10=True,load_samples=False,
                  interpk=1):
@@ -31,8 +28,7 @@ class Green15(DustMap3D):
         HISTORY:
            2015-03-02 - Started - Bovy (IAS)
         """
-        DustMap3D.__init__(self,filter=filter)
-        self._sf10= sf10
+        HierarchicalHealpixMap.__init__(self,filter=filter,sf10=sf10)
         #Read the map
         with h5py.File(os.path.join(_greendir,'dust-map-3d.h5'),'r') \
                 as greendata:
@@ -73,104 +69,4 @@ class Green15(DustMap3D):
         self._intps= numpy.zeros(len(self._pix_info['healpix_index']),
                                  dtype='object') #array to cache interpolated extinctions
         return None
-
-    def _evaluate(self,l,b,d):
-        """
-        NAME:
-           _evaluate
-        PURPOSE:
-           evaluate the dust-map
-        INPUT:
-           l- Galactic longitude (deg)
-           b- Galactic latitude (deg)
-           d- distance (kpc) can be array
-        OUTPUT:
-           extinction E(B-V)
-        HISTORY:
-           2015-03-02 - Started - Bovy (IAS)
-        """
-        distmod= 5.*numpy.log10(d)+10.
-        if isinstance(l,numpy.ndarray) or isinstance(b,numpy.ndarray):
-            raise NotImplementedError("array input for l and b for Green et al. dust map not implemented")
-        lbIndx= self._lbIndx(l,b)
-        if self._intps[lbIndx] != 0:
-            out= self._intps[lbIndx][0](distmod)
-        else:
-            interpData=\
-                interpolate.InterpolatedUnivariateSpline(self._distmods,
-                                                         self._best_fit[lbIndx],
-                                                         k=self._interpk)
-            out= interpData(distmod)
-            self._intps[lbIndx]= interpData
-        if self._filter is None:
-            return out
-        else:
-            return out*aebv(self._filter,sf10=self._sf10)
-
-    def dust_vals_disk(self,lcen,bcen,dist,radius):
-        """
-        NAME:
-           dust_vals_disk
-        PURPOSE:
-           return the distribution of extinction within a small disk as samples
-        INPUT:
-           lcen, bcen - Galactic longitude and latitude of the center of the disk (deg)
-           dist - distance in kpc
-           radius - radius of the disk (deg)
-        OUTPUT:
-           (pixarea,extinction) - arrays of pixel-area in sq rad and extinction value
-        HISTORY:
-           2015-03-06 - Written - Bovy (IAS)
-        """
-        # Convert the disk center to a HEALPIX vector
-        vec= healpy.pixelfunc.ang2vec((90.-bcen)*_DEGTORAD,lcen*_DEGTORAD)
-        distmod= 5.*numpy.log10(dist)+10.
-        # Query the HEALPIX map for pixels that lie within the disk
-        pixarea= []
-        extinction= []
-        for nside in self._nsides:
-            # Find the pixels at this resolution that fall within the disk
-            ipixs= healpy.query_disc(nside,vec,radius*_DEGTORAD,
-                                    inclusive=False,nest=True)
-            # Get indices of all pixels within the disk at current nside level
-            nsideindx= self._pix_info['nside'] == nside
-            potenIndxs= self._indexArray[nsideindx]
-            nsidepix= self._pix_info['healpix_index'][nsideindx]
-            # Loop through the pixels in the (small) disk
-            tout= []
-            for ii,ipix in enumerate(ipixs):
-                lbIndx= potenIndxs[ipix == nsidepix]
-                if numpy.sum(lbIndx) == 0: continue
-                if self._intps[lbIndx] != 0:
-                    tout.append(self._intps[lbIndx][0](distmod))
-                else:
-                    interpData=\
-                        interpolate.InterpolatedUnivariateSpline(self._distmods,
-                                                                 self._best_fit[lbIndx],
-                                                                 k=self._interpk)
-                    tout.append(interpData(distmod))
-                    self._intps[lbIndx]= interpData
-            tarea= healpy.pixelfunc.nside2pixarea(nside)
-            tarea= [tarea for ii in range(len(tout))]
-            pixarea.extend(tarea)
-            extinction.extend(tout)
-        pixarea= numpy.array(pixarea)
-        extinction= numpy.array(extinction)
-        if not self._filter is None:
-            extinction= extinction*aebv(self._filter,sf10=self._sf10)        
-        return (pixarea,extinction)
-
-    def _lbIndx(self,l,b):
-        """Return the index in the _greendata array corresponding to this (l,b)"""
-        for nside in self._nsides:
-            # Search for the pixel in this Nside level
-            tpix= healpy.pixelfunc.ang2pix(nside,(90.-b)*_DEGTORAD,
-                                           l*_DEGTORAD,nest=True)
-            indx= (self._pix_info['healpix_index'] == tpix)\
-                *(self._pix_info['nside'] == nside)
-            if numpy.sum(indx) == 1:
-                return self._indexArray[indx]
-            elif numpy.sum(indx) > 1:
-                raise IndexError("Given (l,b) pair has multiple matches!")
-        raise IndexError("Given (l,b) pair not within the region covered by the Green et al. (2015) dust map")
 
