6207779fc9155d17b36eaab0d18aa02e58d7284f
Miles Lucas <mdlucas@hawaii.edu>
2021-01-14 15:32:54 -0600

    fix missing include

--- mwdust/util/SFD_CodeC/subs_fits.c
+++ mwdust/util/SFD_CodeC/subs_fits.c
@@ -14,6 +14,7 @@
 /******************************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <math.h>
 #include <ctype.h> /* For function toupper() */
