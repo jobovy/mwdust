d360aee75f892ddc5ec302933341d4418ac11ca2
Jo Bovy <bovy@astro.utoronto.ca>
2025-10-06 11:14:27 -0400

    Bump version to next release

--- mwdust/__init__.py
+++ mwdust/__init__.py
@@ -10,7 +10,7 @@ from mwdust.Combined19 import Combined19
 from mwdust.Zucker25 import Zucker25
 from mwdust.Zero import Zero
 
-__version__ = "1.6.dev0"
+__version__ = "1.6"
 
 
 def download_all(test=False):
