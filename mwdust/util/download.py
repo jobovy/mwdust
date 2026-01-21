ee7c5a280142032ed6f04467d72e5dfc3ad78531
Jo Bovy <bovy@astro.utoronto.ca>
2026-01-21 09:37:16 -0500

    Update user-agent to avoid 403 errors

--- mwdust/util/download.py
+++ mwdust/util/download.py
@@ -10,23 +10,32 @@ if dust_dir is None:
 if not os.path.exists(dust_dir):
     os.mkdir(dust_dir)
 
+
 def downloader(url, fullfilename, name, test=False):
     """
     url: URL of data file
     fullfilename: full local path
     name: name of the task
     """
-    user_agent = "Mozilla/5.0"
-    r = requests.get(url, stream=True, allow_redirects=True, verify=True, headers={"User-Agent": user_agent})
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+    r = requests.get(
+        url,
+        stream=True,
+        allow_redirects=True,
+        verify=True,
+        headers={"User-Agent": user_agent},
+    )
     if r.status_code == 404:
         raise ConnectionError(f"Cannot find {name} data file at {url}")
     r.raise_for_status()  # Will only raise for 4xx codes
     if not test:
-        file_size = int(r.headers.get('Content-Length', 0))
+        file_size = int(r.headers.get("Content-Length", 0))
         path = pathlib.Path(fullfilename).expanduser().resolve()
         path.parent.mkdir(parents=True, exist_ok=True)
 
         # r.raw.read
-        with tqdm.tqdm.wrapattr(r.raw, "read", total=file_size, desc=f"Download {name}") as r_raw:
+        with tqdm.tqdm.wrapattr(
+            r.raw, "read", total=file_size, desc=f"Download {name}"
+        ) as r_raw:
             with path.open("wb") as f:
                 shutil.copyfileobj(r_raw, f)
